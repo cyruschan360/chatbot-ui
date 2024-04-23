2a11c953ec6eaf15b099763d8d4798e2d0c7c6ed
Cyrus Chan <cyrus.chan@star.hku.hk>
2024-04-23 11:59:02 +0100

    Activate Llama3-8b-instruct

---
+++ launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#HF_MODEL_ID=TheBloke/Llama-2-70B-chat-AWQ DEVICE=cuda docker compose up -d --build
+HF_MODEL_ID=NousResearch/Meta-Llama-3-8B-Instruct-GGUF DEVICE=cuda docker compose up -d --build
