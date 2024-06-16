3f4f262d76c1002af1d9917f989856482812c473
Cyrus Chan <cyrus.chan@star.hku.hk>
2024-06-16 13:50:36 +0100

    Use cognitivecomputations/dolphin-2.9-llama3-8b-256k

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #HF_MODEL_ID=TheBloke/Llama-2-70B-chat-AWQ DEVICE=cuda docker compose up -d --build
-HF_MODEL_ID=NousResearch/Meta-Llama-3-8B-Instruct-GGUF DEVICE=cuda docker compose up -d --build
+HF_MODEL_ID=meta-llama/Meta-Llama-3-8B-Instruct DEVICE=cuda docker compose up -d --build
